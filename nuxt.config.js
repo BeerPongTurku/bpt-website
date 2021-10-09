ff71c617e6d3b19b87203de7481ebe147724a2e6
Jarkko Pietilä <jarkko.pietila@live.fi>
2021-10-09 18:45:50 +0300

    Update Nuxt configuration

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,26 +2,34 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  // Disable server-side rendering (https://go.nuxtjs.dev/ssr-mode)
+  ssr: false,
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'bpt-website',
+    title: 'Beer Pong Turku ry',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
+      { hid: 'description', name: 'description', content: 'Academic Beer Pong association in Turku' },
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/png', href: '/favicon.png' }
     ]
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
   ],
+
+  // Global SCSS variables
+  styleResources: {
+    scss: ['./assets/styles/variables.scss']
+  },
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
@@ -49,5 +57,12 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
+  },
+
+  // Loading indicator
+  loadingIndicator: {
+    name: 'three-bounce',
+    color: 'white',
+    background: '#154506'
   }
 }
