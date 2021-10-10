436f24034df0c6a279b94e67e657ebc67c2bd5db
Jarkko Pietilä <jarkko.pietila@live.fi>
2021-10-10 19:43:54 +0300

    Scan components from a sub-directory

--- nuxt.config.js
+++ nuxt.config.js
@@ -39,7 +39,10 @@ export default {
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
+  components: [
+    '~/components',
+    '~/components/events',
+  ],
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
