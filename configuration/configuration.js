da048a7615b6d2736bca7836118c2bdaac065449
Jarkko Pietilä <jarkko.pietila@solita.fi>
2022-10-23 19:04:30 +0300

    Add Google Calendar integration

--- configuration/configuration.js
+++ configuration/configuration.js
@@ -1,5 +1,35 @@
-import configuration from './configuration.json';
+import configuration from './configuration.json'
 
-export default function navigationLinks() {
-    return configuration.navigationLinks;
+export function navigationLinks() {
+  return configuration.navigationLinks
+}
+
+export function eventsRequestURI() {
+  const { id, key } = configuration.calendar
+
+  const params = new URLSearchParams({
+    maxResults: 3,
+    singleEvents: true,
+    timeMin: new Date().toISOString(),
+    orderBy: 'startTime',
+    key,
+  });
+  
+  const url = new URL('https://www.googleapis.com/calendar/v3/calendars/' + id + '/events')
+  url.search = params
+
+  return url.toString()
+}
+
+export function locationLinkRequestURI(location) {
+
+  const params = new URLSearchParams({
+    api: 1,
+    query: location
+  });
+
+  const url = new URL('https://www.google.com/maps/search/')
+  url.search = params
+
+  return url.toString()
 }
